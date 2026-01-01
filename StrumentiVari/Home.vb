e5a4f1bb4b2fd3c6cda66af1c3095ddf80d34cb0
sosp93 <eros1@PC-EROS-21>
2026-01-01 12:30:37 +0100

    Aggiungere i file di progetto.

---
+++ StrumentiVari/Home.vb
@@ -0,0 +1,5 @@
+﻿Public Class Home
+    Private Sub BtnSaluta_Click(sender As Object, e As EventArgs) Handles BtnSaluta.Click
+        MsgBox("Benvenuto!", vbOKOnly, "Placeholder")
+    End Sub
+End Class
