58206c3edce38e06d39996de8e3b771d05182cf8
sosp93 <eros1993@hotmail.it>
2026-02-08 17:30:29 +0100

    Predisposizione classe Taratura
    Prima versione della Classe Taratura senza alcun controllo di validità degli input

---
+++ StrumentiVari/Taratura.vb
@@ -0,0 +1,28 @@
+﻿Public Class Taratura
+    Private zeroDesiderato As Double
+    Private fsDesiderato As Double
+    Private diffDesiderata As Double
+
+    Public Sub New(zero As Double, fondoscala As Double)
+        Me.zeroDesiderato = zero
+        Me.fsDesiderato = fondoscala
+        Me.diffDesiderata = fsDesiderato - zeroDesiderato
+    End Sub
+
+    Private Function getRapportoTaratura(zeroAttuale As Double, fsAttuale As Double)
+        Return diffDesiderata / (fsAttuale - zeroAttuale)
+    End Function
+
+    Public Function daTrovareFs(zeroAttuale As Double, fsAttuale As Double)
+        'Dim diffAttuale As Double
+        'diffAttuale = fsAttuale - zeroAttuale
+        'Return fsAttuale / diffAttuale * diffDesiderata
+        Return fsAttuale * getRapportoTaratura(zeroAttuale, fsAttuale)
+    End Function
+
+    Public Function daTrovareZero(zeroAttuale As Double, fsAttuale As Double)
+        Return zeroAttuale * getRapportoTaratura(zeroAttuale, fsAttuale)
+    End Function
+
+
+End Class
