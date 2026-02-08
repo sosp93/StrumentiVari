75d3e774f7f1b57e581917daf44cad90b0c18cf5
sosp93 <eros1993@hotmail.it>
2026-02-08 22:00:34 +0100

    Aggiunta restituizione valori in formato stringa
    Valori in formato stringa con impostazione del numero di cifre decimali manualmente, di default 3 cifre decimali

--- StrumentiVari/Taratura.vb
+++ StrumentiVari/Taratura.vb
@@ -2,11 +2,13 @@
     Private zeroDesiderato As Double
     Private fsDesiderato As Double
     Private diffDesiderata As Double
+    Private stringaFormatoValori As String
 
     Public Sub New(zero As Double, fondoscala As Double)
         Me.zeroDesiderato = zero
         Me.fsDesiderato = fondoscala
         Me.diffDesiderata = fsDesiderato - zeroDesiderato
+        Me.stringaFormatoValori = "0.000"
     End Sub
     Public Sub New(zeroString As String, fondoscalaString As String)
         If Not IsNumeric(zeroString) Then Throw New System.Exception("Zero non è un valore numerico")
@@ -14,16 +16,31 @@
         Me.zeroDesiderato = CDbl(zeroString)
         Me.fsDesiderato = CDbl(fondoscalaString)
         Me.diffDesiderata = fsDesiderato - zeroDesiderato
+        Me.stringaFormatoValori = "0.000"
     End Sub
+
+    Public Function setDecimali(cifre As Short) As Boolean
+        Dim i As Short
+        Dim s As String
+        If cifre = 0 Then
+            s = "0"
+            Return True
+        ElseIf cifre >= 0 Then
+            s = "0."
+            For i = 1 To cifre
+                s &= "0"
+            Next
+            Return True
+        End If
+        Return False
+    End Function
 
     Private Function getRapportoTaratura(zeroAttuale As Double, fsAttuale As Double) As Double
         Return diffDesiderata / (fsAttuale - zeroAttuale)
     End Function
 
+    'restituiscono i valori calcolati in formato numerico
     Public Function daTrovareFs(zeroAttuale As Double, fsAttuale As Double) As Double
-        'Dim diffAttuale As Double
-        'diffAttuale = fsAttuale - zeroAttuale
-        'Return fsAttuale / diffAttuale * diffDesiderata
         Return fsAttuale * getRapportoTaratura(zeroAttuale, fsAttuale)
     End Function
 
@@ -31,5 +48,12 @@
         Return zeroAttuale * getRapportoTaratura(zeroAttuale, fsAttuale)
     End Function
 
+    'Restituiscono valori calcolati in stringa con quantità decimali impostata
+    Public Function daTrovareFsString(zeroAttuale As Double, fsAttuale As Double)
+        Return daTrovareFs(zeroAttuale, fsAttuale).ToString(stringaFormatoValori)
+    End Function
+    Public Function daTrovareZeroString(zeroAttuale As Double, fsAttuale As Double) As String
+        Return daTrovareZero(zeroAttuale, fsAttuale).ToString(stringaFormatoValori)
+    End Function
 
 End Class
